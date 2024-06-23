c180212f66d3f64ff4ba1f7774913ad34e2c7d9d
19746 <mattiabraga2006@gmail.com>
2024-06-23 23:07:33 +0200

    finished css for main page and status page. Done all the search_books functions

--- bin/utils.py
+++ bin/utils.py
@@ -2,8 +2,18 @@
 Neetre 2024
 '''
 
+import argparse
+
 from DataManager import DatabaseManager_books
 from html_templates import *
+
+
+def args_parsing():
+    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+    parser.add_argument("-l", '--local', action="store_true", default=False,
+                        help='Host the website localy')
+    parser.add_argument("-v", '--verbose', action="store_true", default=False,
+                        help='Prints everything')
 
 class Book:
     def __init__(self, title, author, publisher, year, genre, type, code, link_cover="", desciption=""):
@@ -69,6 +79,21 @@ def create_books_webpage():
         
         f.write(FOOTER)
     print("Books webpage created")
+    
+
+def search_books_webpage(search):
+    db = DatabaseManager_books()
+    books = db.read_books()
+    
+    with open("./templates/books.html", "w") as f:
+        f.write(HEADER)
+        
+        for book in books:
+            if search in [book[n] for n in range(len(book)-1)] or search in [book[n].lower() for n in range(len(book)-1)]:
+                f.write(f"<tr><td><a href='/load_book?code={book[7]}'>{book[1]}</a></td><td>{book[2]}</td><td>{book[3]}</td><td>{book[4]}</td><td>{book[5]}</td><td>{book[6]}</td><td>{book[7]}</td></tr>")
+        f.write("\t</table>")
+        f.write(HOME)
+        f.write(FOOTER)
 
 
 def search_books_webpage_author(author):
@@ -81,7 +106,6 @@ def search_books_webpage_author(author):
         for book in books:
             if author in book[2] or author in book[2].lower():
                 f.write(f"<tr><td><a href='/load_book?code={book[7]}'>{book[1]}</a></td><td>{book[2]}</td><td>{book[3]}</td><td>{book[4]}</td><td>{book[5]}</td><td>{book[6]}</td><td>{book[7]}</td></tr>")
-        
         f.write("\t</table>")
         f.write(HOME)
         f.write(FOOTER)
@@ -97,6 +121,11 @@ def search_books_webpage_genre(genre):
         for book in books:
             if genre in book[5] or genre in book[5].lower():
                 f.write(f"<tr><td><a href='/load_book?code={book[7]}'>{book[1]}</a></td><td>{book[2]}</td><td>{book[3]}</td><td>{book[4]}</td><td>{book[5]}</td><td>{book[6]}</td></tr>")
+        f.write("\t</table>")
+        f.write(HOME)
+        f.write(FOOTER)
+        
+    print("Books webpage created")
 
 def search_books_webpage_type(type):
     db = DatabaseManager_books()
@@ -104,11 +133,12 @@ def search_books_webpage_type(type):
     
     with open("./templates/books.html", "w") as f:
         f.write(HEADER)
-        
 
-def search_books_webpage(seach):
-    db = DatabaseManager_books()
-    books = db.read_books()
+        for book in books:
+            if type in book[6] or type in book[6].lower():
+                f.write(f"<tr><td><a href='/load_book?code={book[7]}'>{book[1]}</a></td><td>{book[2]}</td><td>{book[3]}</td><td>{book[4]}</td><td>{book[5]}</td><td>{book[6]}</td></tr>")
+        f.write("\t</table>")
+        f.write(HOME)
+        f.write(FOOTER)
     
-    with open("./templates/books.html", "w") as f:
-        f.write(HEADER)
+    print("Books webpage created")
--- bin/webpage.py
+++ bin/webpage.py
@@ -1,2 +1,35 @@
+'''
+
+Neetre 2024
+'''
+
 from flask import Flask, render_template, request
 from utils import *
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def books_page():
+    create_books_webpage()
+    return render_template('books.html')
+
+
+@app.route('/home_page')
+def home_page():
+    create_books_webpage()
+    return render_template('books.html')
+
+
+@app.route('/search_books', methods=['POST', 'GET'])
+def search_book():
+    if request.method == 'POST':
+        search = request.form['search']
+    else:
+        search = request.args.get('search')
+    search_books_webpage(search)
+    return render_template('books.html')
+
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=5000, debug=True)
