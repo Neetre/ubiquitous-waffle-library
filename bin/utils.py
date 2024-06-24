7976d94722e46f652ad71ddbbf6060774b68f2d4
Mattia Braga <mattiabraga2006@gmail.com>
2024-06-24 09:09:33 +0200

    optimization right now, not later

--- bin/utils.py
+++ bin/utils.py
@@ -3,6 +3,7 @@ Neetre 2024
 '''
 
 import argparse
+from icecream import ic
 
 from DataManager import DatabaseManager_books
 from html_templates import *
@@ -15,9 +16,30 @@ def args_parsing():
     parser.add_argument("-v", '--verbose', action="store_true", default=False,
                         help='Prints everything')
 
+
+    args = parser.parse_args()
+    return args
+
+
+class Library:
+    def __init__(self, name, location):
+        self.name = name
+        self.location = location
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    def remove_book(self, book):
+        self.books.remove(book)
+
+    def __str__(self) -> str:
+        return f"{self.name} located in {self.location} with {len(self.books)} books"
+
+
 class Book:
     def __init__(self, title, author, publisher, year, genre, type, code, link_cover="", desciption=""):
-        self.tile = title
+        self.title = title
         self.author = author
         self.publisher = publisher
         self.year = year
@@ -27,6 +49,9 @@ class Book:
         self.link_cover = link_cover
         self.desciption = desciption
 
+    def __str__(self):
+        return f"{self.title} by {self.author} published by {self.publisher} in {self.year} - {self.genre} - {self.type} - {self.code}"
+
 
 class BookStatus:
     def __init__(self, book: Book, status, date, note):
@@ -34,6 +59,17 @@ class BookStatus:
         self.status = status
         self.date = date
         self.note = note
+
+    def __str__(self):
+        return f"{self.book.title} - {self.status} - {self.date} - {self.note}"
+
+
+def read_books():
+    db = DatabaseManager_books()
+    books = db.read_books()
+    library = Library("Library", "Rome")
+    library.add_book([Book(book[1], book[2], book[3], book[4], book[5], book[6], book[7], book[8], book[9]) for book in books])
+    return library
 
 
 def create_books_webpage():
@@ -78,7 +114,7 @@ def create_books_webpage():
         f.write("</section>")
         
         f.write(FOOTER)
-    print("Books webpage created")
+    ic("Books webpage created")
     
 
 def search_books_webpage(search):
@@ -95,6 +131,8 @@ def search_books_webpage(search):
         f.write(HOME)
         f.write(FOOTER)
 
+    ic("Books webpage created")
+
 
 def search_books_webpage_author(author):
     db = DatabaseManager_books()
@@ -109,6 +147,8 @@ def search_books_webpage_author(author):
         f.write("\t</table>")
         f.write(HOME)
         f.write(FOOTER)
+
+    ic("Books webpage created")
 
 
 def search_books_webpage_genre(genre):
@@ -125,7 +165,7 @@ def search_books_webpage_genre(genre):
         f.write(HOME)
         f.write(FOOTER)
         
-    print("Books webpage created")
+    ic("Books webpage created")
 
 def search_books_webpage_type(type):
     db = DatabaseManager_books()
@@ -141,4 +181,42 @@ def search_books_webpage_type(type):
         f.write(HOME)
         f.write(FOOTER)
     
-    print("Books webpage created")
+    ic("Books webpage created")
+
+
+def search_books_webpage_year(year):
+    db = DatabaseManager_books()
+    books = db.read_books()
+    
+    with open("./templates/books.html", "w") as f:
+        f.write(HEADER)
+
+        for book in books:
+            if type in book[6] or type in book[6].lower():
+                f.write(f"<tr><td><a href='/load_book?code={book[7]}'>{book[1]}</a></td><td>{book[2]}</td><td>{book[3]}</td><td>{book[4]}</td><td>{book[5]}</td><td>{book[6]}</td></tr>")
+        f.write("\t</table>")
+        f.write(HOME)
+        f.write(FOOTER)
+    
+    ic("Books webpage created")
+
+
+def add_book_webpage(title, author, publisher, year, genre, type):
+    db = DatabaseManager_books()
+    
+    code = title[:3].upper() + author[:3].upper() + publisher[:3] + str(year)
+    book = Book(title, author, publisher, year, genre, type, code)
+    db.write_book(book)
+    create_books_webpage()
+
+
+def write_book_test():
+    book = Book("Test", "Test", "Test", 2021, "Test", "Test", "TST")
+    library = read_books()
+    library.add_book(book)
+    for book in library.books:
+        print(book)
+
+
+if __name__ == "__main__":
+    write_book_test()
--- bin/webpage.py
+++ bin/webpage.py
@@ -9,6 +9,19 @@ from utils import *
 app = Flask(__name__)
 
 
+def handle_search(search_term, search_type):
+    if search_type == 'title':
+        search_books_webpage(search_term)
+    elif search_type == 'author':
+        search_books_webpage_author(search_term)
+    elif search_type == 'genre':
+        search_books_webpage_genre(search_term)
+    elif search_type == 'type':
+        search_books_webpage_type(search_term)
+    elif search_type == 'year':
+        search_books_webpage_year(search_term)
+
+
 @app.route('/')
 def books_page():
     create_books_webpage()
@@ -17,17 +30,44 @@ def books_page():
 
 @app.route('/home_page')
 def home_page():
-    create_books_webpage()
+    books_page()
+
+
+@app.route('/search', methods=['POST', 'GET'])
+def search():
+    if request.method == 'POST':
+        search = request.form['search']
+        search_type = request.form['search_type']
+    else:
+        search = request.args.get('search')
+        search_type = request.args.get('search_type')
+    handle_search(search. search_type)
     return render_template('books.html')
 
 
-@app.route('/search_books', methods=['POST', 'GET'])
-def search_book():
+@app.route('/add_book', methods=['POST', 'GET'])
+def add_book():
     if request.method == 'POST':
-        search = request.form['search']
+        title = request.form['title']
+        surname = request.form['surname']
+        name = request.form['name']
+        publisher = request.form['publisher']
+        year = request.form['year']
+        genre = request.form['genre']
+        type = request.form['type']
+        author = f"{surname} {name}"
+        ic(title, author)
     else:
-        search = request.args.get('search')
-    search_books_webpage(search)
+        title = request.args.get('title')
+        surname = request.args.get('author')
+        name = request.args.get('name')
+        publisher = request.args.get('publisher')
+        year = request.args.get('year')
+        genre = request.args.get('genre')
+        type = request.args.get('type')
+        author = f"{surname} {name}"
+        ic(title, author)
+    add_book_webpage(title, author, publisher, year, genre, type)
     return render_template('books.html')
 
 
